ac8091f1a5f8f12893cd530e13f3a0112b1dbc86
BoomStar <73805598+BoomStarcuc@users.noreply.github.com>
2024-01-14 18:36:48 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -50,3 +50,32 @@ document.querySelectorAll('.carousel-method-segment').forEach(segment => {
       document.getElementById('selected-image').src = imagePath;
   });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const carousel = document.querySelector(".carousel-bar");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  carousel.addEventListener('mousedown', (e) => {
+      isDown = true;
+      carousel.classList.add('active');
+      startX = e.pageX - carousel.offsetLeft;
+      scrollLeft = carousel.scrollLeft;
+  });
+  carousel.addEventListener('mouseleave', () => {
+      isDown = false;
+      carousel.classList.remove('active');
+  });
+  carousel.addEventListener('mouseup', () => {
+      isDown = false;
+      carousel.classList.remove('active');
+  });
+  carousel.addEventListener('mousemove', (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - carousel.offsetLeft;
+      const walk = (x - startX) * 3; //scroll-fast
+      carousel.scrollLeft = scrollLeft - walk;
+  });
+});
